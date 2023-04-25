60bf18d5906e015697cb883884c92508f59062a2
Cao Zhong Z <zhong.z.cao@intel.com>
2023-04-25 13:14:06 +0800

    Add layer_norm tests
    Signed-off-by: Cao Zhong Z <zhong.z.cao@intel.com>

--- deepspeed/main.cpp
+++ deepspeed/main.cpp
@@ -49,6 +49,45 @@ void test_softmax(int batch, int heads, int num_seq, int soft_seq) {
   sycl::free(host_o, q);
 }
 
+template <typename T>
+void test_layernorm(int rows, int elems_per_row) {
+  auto q = currentQueue();
+
+  auto elem = rows * elems_per_row;
+  auto size = elem * sizeof(T);
+  auto row_sz = elems_per_row * sizeof(T);
+
+  auto* vals = (T *)sycl::malloc_device(size, q);
+  auto* output = (T *)sycl::malloc_device(size, q);
+
+  auto* gamma = (T *)sycl::malloc_device(row_sz, q);
+  auto* beta = (T *)sycl::malloc_device(row_sz, q);
+
+  auto* host_in = (T *)sycl::malloc_host(size, q);
+  auto* host_o = (T *)sycl::malloc_host(size, q);
+
+  for (size_t i = 0; i < elem; ++ i) {
+    host_in[i] = T(1.0);
+  }
+
+  q.memcpy(vals, host_in, size);
+  q.memset(gamma, T(1.0), row_sz);
+  q.memset(beta, T(0.0), row_sz);
+
+  launch_fused_ln(output, vals, gamma, beta, 0.00001, rows, elems_per_row, q);
+
+  q.memcpy(host_o, vals, size);
+  q.wait();
+
+  sycl::free(vals, q);
+  sycl::free(output, q);
+  sycl::free(gamma, q);
+  sycl::free(beta, q);
+  sycl::free(host_in, q);
+  sycl::free(host_o, q);
+}
+
+
 template <typename T, typename F>
 T test_conversion(F val) {
   return conversion::to<T>(val);
@@ -71,8 +110,9 @@ int main(int argc, char ** argv) {
   float f_standard = 1.0;
   sycl::half converted_f_standard = conversion::to<sycl::half>(f_standard);
 
-  test_softmax<sycl::half>(batch_size, heads, num_seq, soft_seq);
-  test_softmax<bf16>(batch_size, heads, num_seq, soft_seq);
+  test_layernorm(batch_size, heads);
+  // test_softmax<sycl::half>(batch_size, heads, num_seq, soft_seq);
+  // test_softmax<bf16>(batch_size, heads, num_seq, soft_seq);
 
   return 0;
 }
